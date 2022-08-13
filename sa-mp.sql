4c62163dff87b13243bb605df1b747b0e624a6d6
Giapp <vdgiapp@gmail.com>
2022-08-14 02:08:03 +0700

    Web updated

--- sa-mp.sql
+++ sa-mp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th8 11, 2022 lúc 09:34 AM
+-- Thời gian đã tạo: Th8 13, 2022 lúc 09:04 PM
 -- Phiên bản máy phục vụ: 10.4.24-MariaDB
 -- Phiên bản PHP: 8.1.6
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `sap-rp`
+-- Cơ sở dữ liệu: `sa-mp`
 --
 
 -- --------------------------------------------------------
@@ -30,12 +30,16 @@ SET time_zone = "+00:00";
 CREATE TABLE `accounts` (
   `UID` int(11) NOT NULL,
   `Account` varchar(32) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `DisplayName` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
   `Online` int(11) NOT NULL,
+  `Confirm` int(11) NOT NULL,
+  `Token` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Email` varchar(64) COLLATE utf8mb4_vietnamese_ci NOT NULL,
   `Password` varchar(65) COLLATE utf8mb4_vietnamese_ci NOT NULL,
   `Password2` varchar(65) COLLATE utf8mb4_vietnamese_ci NOT NULL,
   `EnablePass2` int(11) NOT NULL,
-  `Email` varchar(64) COLLATE utf8mb4_vietnamese_ci NOT NULL,
   `BanData` varchar(128) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Birthday` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
   `BanReason` varchar(512) COLLATE utf8mb4_vietnamese_ci NOT NULL,
   `LastTimeLogged` varchar(32) COLLATE utf8mb4_vietnamese_ci NOT NULL,
   `DateCreated` varchar(32) COLLATE utf8mb4_vietnamese_ci NOT NULL
@@ -45,11 +49,9 @@ CREATE TABLE `accounts` (
 -- Đang đổ dữ liệu cho bảng `accounts`
 --
 
-INSERT INTO `accounts` (`UID`, `Account`, `Online`, `Password`, `Password2`, `EnablePass2`, `Email`, `BanData`, `BanReason`, `LastTimeLogged`, `DateCreated`) VALUES
-(1, 'vdgiapp', 0, '47A098BEC5DF36D2ACE0AACBB6BA9A83907298619913393CD19F489E200046A6', '66595AF2140492D5D19A980352DD16D491E62067A8FAC4D1B3D8C84617C70BE0', 0, 'vdgiapp@gmail.com', '0 0 0 0 none 0 0 0', '', '14 30 54 11 08 2022', ''),
-(2, 'redshank', 0, '47A098BEC5DF36D2ACE0AACBB6BA9A83907298619913393CD19F489E200046A6', '', 0, 'redshankseries@gmail.com', '0 0 0 0 none 0 0 0', '', '08 28 15 04 08 2022', ''),
-(3, 'minecraft', 0, '47A098BEC5DF36D2ACE0AACBB6BA9A83907298619913393CD19F489E200046A6', '', 0, 'minecraft151607389@gmail.com', '0 0 0 0 none 0 0 0', '', '22 17 30 29 07 2022', ''),
-(4, 'android', 0, '47A098BEC5DF36D2ACE0AACBB6BA9A83907298619913393CD19F489E200046A6', '', 0, 'tuilagiaptbz@gmail.com', '0 0 0 0 none 0 0 0', '', '08 25 33 04 08 2022', '');
+INSERT INTO `accounts` (`UID`, `Account`, `DisplayName`, `Online`, `Confirm`, `Token`, `Email`, `Password`, `Password2`, `EnablePass2`, `BanData`, `Birthday`, `BanReason`, `LastTimeLogged`, `DateCreated`) VALUES
+(1, 'vdgiapp', 'Giapp', 0, 1, 'HOMMc9bwTXaTcmUqFsFNM7EwPEfoNsNk', 'vdgiapp@gmail.com', '47a098bec5df36d2ace0aacbb6ba9a83907298619913393cd19f489e200046a6', '66595af2140492d5d19a980352dd16d491e62067a8fac4d1b3d8c84617c70be0', 0, '0 0 0 0 none 0 0 0', '09 11 2004', '', '02 02 17 14 08 2022', ' 01 01 2022'),
+(6, 'redshank', 'Tóc đỏ', 0, 0, 'hc1hlcNYSfJGD683u2YKifyH40YtnQqI', 'redshankseries@gmail.com', '47a098bec5df36d2ace0aacbb6ba9a83907298619913393cd19f489e200046a6', '', 0, '', '', '', '01 28 13 14 08 2022', '14 08 2022');
 
 -- --------------------------------------------------------
 
@@ -85,6 +87,7 @@ CREATE TABLE `characters` (
   `JobID` int(11) NOT NULL,
   `FightStyle` int(11) NOT NULL,
   `WalkStyle` int(11) NOT NULL,
+  `WeaponSkills` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
   `Playtime` int(11) NOT NULL,
   `Payday` int(11) NOT NULL,
   `RespawnLocation` int(11) NOT NULL,
@@ -109,19 +112,75 @@ CREATE TABLE `characters` (
   `PhoneData` varchar(32) COLLATE utf8mb4_vietnamese_ci NOT NULL,
   `ContactData` varchar(1024) COLLATE utf8mb4_vietnamese_ci NOT NULL,
   `WalkieTalkieData` varchar(64) COLLATE utf8mb4_vietnamese_ci NOT NULL,
-  `Settings` varchar(128) COLLATE utf8mb4_vietnamese_ci NOT NULL
+  `Settings` varchar(128) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item0` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item1` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item2` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item3` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item4` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item5` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item6` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item7` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item8` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item9` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item10` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item11` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item12` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item13` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item14` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item15` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item16` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item17` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item18` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item19` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item20` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item21` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item22` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item23` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item24` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item25` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item26` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item27` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item28` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item29` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item30` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item31` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item32` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item33` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item34` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item35` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item36` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item37` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item38` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item39` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item40` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item41` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item42` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item43` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item44` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item45` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item46` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item47` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item48` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item49` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item50` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item51` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item52` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item53` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item54` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item55` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item56` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item57` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item58` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `Item59` text COLLATE utf8mb4_vietnamese_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `characters`
 --
 
-INSERT INTO `characters` (`#`, `Account`, `Slot`, `Name`, `DateCreated`, `LastTimePlayed`, `Birthday`, `Gender`, `Nation`, `Description`, `SkinID`, `Level`, `Respects`, `JobID`, `FightStyle`, `WalkStyle`, `Playtime`, `Payday`, `RespawnLocation`, `Faction`, `Warns`, `Wanted`, `Arrested`, `Hunger`, `Thirst`, `Stamina`, `Injured`, `Health`, `Armour`, `Position`, `ImprisonData`, `MuteData`, `Cash`, `Coins`, `Bank`, `IDCard`, `LicenseData`, `PhoneData`, `ContactData`, `WalkieTalkieData`, `Settings`) VALUES
-(1, 'vdgiapp', 1, 'GetPlayerName', '21 48 54 02 08 2022', '21 48 54 02 08 2022', '09 11 1995', 1, 1, '', 2, '1 0', 0, 0, 4, 0, 0, 0, 0, '', 0, 0, 0, 1000, 1000, 1000, 0, 57, 0, '1851.049926 -1853.333984 17.977964 1.101451 0 0', '0 0 0 0', '0 0', 1000, 10, '', 0, '0 0 0 0 0 0', '0 000000 0', '', '', ''),
-(3, 'android', 1, 'Justin_Barber', '23 02 23 02 08 2022', '23 02 23 02 08 2022', '07 08 1987', 1, 1, '', 72, '1 0', 0, 0, 4, 0, 0, 0, 0, '', 0, 0, 0, 1000, 1000, 1000, 0, 100, 0, '1743 -1862 13.6 0 0 0', '0 0 0 0', '0 0', 1000, 10, '', 0, '0 0 0 0 0 0', '0 000000 0', '', '', ''),
-(4, 'redshank', 1, 'Dante_Grimes', '19 34 27 03 08 2022', '19 34 27 03 08 2022', '03 11 1987', 1, 1, '', 23, '1 0', 0, 0, 4, 0, 0, 0, 0, '', 0, 0, 0, 1000, 1000, 1000, 0, 100, 0, '1743 -1862 13.6 0 0 0', '0 0 0 0', '0 0', 1000, 10, '', 0, '0 0 0 0 0 0', '0 000000 0', '', '', ''),
-(5, 'vdgiapp', 2, 'Jay_Holt', '06 19 18 06 08 2022', '06 19 18 06 08 2022', '15 10 1989', 1, 1, '', 2, '1 0', 0, 0, 4, 1, 0, 0, 0, '', 0, 0, 0, 1000, 1000, 1000, 0, 100, 0, '1838.846069 -1842.516113 19.981443 90.000000 0 0', '0 0 0 0', '0 0', 1000, 10, '', 0, '0 0 0 0 0 0', '0 000000 0', '', '', ''),
-(7, 'vdgiapp', 3, 'What_That', '14 05 37 11 08 2022', '14 05 37 11 08 2022', '01 01 1942', 1, 1, '', 2, '1 0', 0, 0, 4, 0, 0, 0, 0, '', 0, 0, 0, 1000, 1000, 1000, 0, 100, 0, '1776.238525 -1865.565307 16.204780 264.662292 0 0', '0 0 0 0', '0 0', 1000, 10, '', 0, '0 0 0 0 0 0', '0 000000 0', '', '', '');
+INSERT INTO `characters` (`#`, `Account`, `Slot`, `Name`, `DateCreated`, `LastTimePlayed`, `Birthday`, `Gender`, `Nation`, `Description`, `SkinID`, `Level`, `Respects`, `JobID`, `FightStyle`, `WalkStyle`, `WeaponSkills`, `Playtime`, `Payday`, `RespawnLocation`, `Faction`, `Warns`, `Wanted`, `Arrested`, `Hunger`, `Thirst`, `Stamina`, `Injured`, `Health`, `Armour`, `Position`, `ImprisonData`, `MuteData`, `Cash`, `Coins`, `Bank`, `IDCard`, `LicenseData`, `PhoneData`, `ContactData`, `WalkieTalkieData`, `Settings`, `Item0`, `Item1`, `Item2`, `Item3`, `Item4`, `Item5`, `Item6`, `Item7`, `Item8`, `Item9`, `Item10`, `Item11`, `Item12`, `Item13`, `Item14`, `Item15`, `Item16`, `Item17`, `Item18`, `Item19`, `Item20`, `Item21`, `Item22`, `Item23`, `Item24`, `Item25`, `Item26`, `Item27`, `Item28`, `Item29`, `Item30`, `Item31`, `Item32`, `Item33`, `Item34`, `Item35`, `Item36`, `Item37`, `Item38`, `Item39`, `Item40`, `Item41`, `Item42`, `Item43`, `Item44`, `Item45`, `Item46`, `Item47`, `Item48`, `Item49`, `Item50`, `Item51`, `Item52`, `Item53`, `Item54`, `Item55`, `Item56`, `Item57`, `Item58`, `Item59`) VALUES
+(1, 'vdgiapp', 1, 'Jay_Holt', '21 48 54 02 08 2022', '21 48 54 02 08 2022', '09 11 1995', 1, 1, '', 2, '1 0', 0, 0, 4, 0, '0 0 0 0 0 0 0 0 0 0 0', 0, 0, 0, '', 0, 0, 0, 1000, 1000, 1000, 0, 33, 0, '1406.650269 -1815.512939 14.933951 50.376701 0 0', '0 0 0 0', '0 0', 1000, 10, '', 0, '0 0 0 0 0 0', '0 000000 0', '', '', '', '47 3 100.0 0 7', '22 1 100.0 48 17', '85 10 100.0 0 0', '23 1 100.0 47 7', '48 5 100.0 0 17', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -149,30 +208,6 @@ CREATE TABLE `furniture` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `inventory`
---
-
-CREATE TABLE `inventory` (
-  `#` int(11) NOT NULL,
-  `CharName` varchar(25) NOT NULL,
-  `ItemSlot` int(11) NOT NULL,
-  `ItemID` int(11) NOT NULL,
-  `Amount` int(11) NOT NULL,
-  `Durable` float NOT NULL,
-  `GunAmmo` int(11) NOT NULL,
-  `MagAmmo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Đang đổ dữ liệu cho bảng `inventory`
---
-
-INSERT INTO `inventory` (`#`, `CharName`, `ItemSlot`, `ItemID`, `Amount`, `Durable`, `GunAmmo`, `MagAmmo`) VALUES
-(1, 'Jay_Holt', 0, 25, 1, 100, 7, 0);
-
--- --------------------------------------------------------
-
---
 -- Cấu trúc bảng cho bảng `serverinfo`
 --
 
@@ -189,7 +224,7 @@ CREATE TABLE `serverinfo` (
 --
 
 INSERT INTO `serverinfo` (`#`, `Registered`, `CharCreated`, `Day`, `Logged`) VALUES
-(1, 4, 5, 11, 7);
+(1, 4, 5, 13, 0);
 
 -- --------------------------------------------------------
 
@@ -260,12 +295,6 @@ ALTER TABLE `furniture`
   ADD PRIMARY KEY (`#`);
 
 --
--- Chỉ mục cho bảng `inventory`
---
-ALTER TABLE `inventory`
-  ADD PRIMARY KEY (`#`);
-
---
 -- Chỉ mục cho bảng `serverinfo`
 --
 ALTER TABLE `serverinfo`
@@ -291,7 +320,7 @@ ALTER TABLE `vehicles`
 -- AUTO_INCREMENT cho bảng `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `UID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `UID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT cho bảng `businesses`
@@ -316,12 +345,6 @@ ALTER TABLE `factions`
 --
 ALTER TABLE `furniture`
   MODIFY `#` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT cho bảng `inventory`
---
-ALTER TABLE `inventory`
-  MODIFY `#` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `serverinfo`
